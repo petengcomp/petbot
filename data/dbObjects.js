44177f6fa109367655e46c2a6d4fd78d616b0777
Lucca Passos Covre <luccapassoscovre@gmail.com>
2020-06-19 00:23:33 -0300

    ?

--- data/dbObjects.js
+++ data/dbObjects.js
@@ -4,10 +4,7 @@ let sequelize
 
 if (process.env.DATABASE_URL) {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
-		dialect: 'postgres',
-		dialectOptions: {
-		  ssl: true
-		}
+		dialect: 'postgres'
 	  })
 } else {
 	sequelize = new Sequelize('database', 'username', 'password', {
